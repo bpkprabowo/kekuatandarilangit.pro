bc977a188a330ac2f519a8378d0af85ee4084583
bpkprabowo <monikaandian1212@gmail.com>
2024-08-28 06:20:50 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -3,8 +3,10 @@
 <a href="https://pmb.perbanas.id/138/">https://pmb.perbanas.id/138/</a>
 <a href="https://pusatprestasinasional.kemdikbud.go.id/nasgor/">https://pusatprestasinasional.kemdikbud.go.id/nasgor/</a>
 <a href="https://jurnal.umsu.ac.id/nasgor/">https://jurnal.umsu.ac.id/nasgor/</a>
+<a href="https://e-journal.stikessatriabhakti.ac.id/rajasloto/">https://e-journal.stikessatriabhakti.ac.id/rajasloto/</a>
 <a href="https://e-journal.stikessatriabhakti.ac.id/taiwan/">https://e-journal.stikessatriabhakti.ac.id/taiwan/</a>
 <a href="https://e-journal.stikessatriabhakti.ac.id/burungbet/">https://e-journal.stikessatriabhakti.ac.id/burungbet/</a>
+<a href="https://si.panda.id/rajasloto/">https://si.panda.id/rajasloto/</a>
 <a href="https://si.panda.id/kamboja/">https://si.panda.id/kamboja/</a>
 <a href="https://si.panda.id/burungbet/">https://si.panda.id/burungbet/</a>
 </div>
