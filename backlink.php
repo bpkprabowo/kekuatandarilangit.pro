fcd14ec40ca870a6152d5063866c93170e394f00
bpkprabowo <monikaandian1212@gmail.com>
2024-09-10 08:44:57 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,13 +1,3 @@
 <div style="display:none">
 <a href="https://pendidikan-fisika.uinsgd.ac.id/burungbet/">https://pendidikan-fisika.uinsgd.ac.id/burungbet/</a>
-<a href="https://pmb.perbanas.id/nasgor/">https://pmb.perbanas.id/nasgor/</a>
-<a href="https://pmb.perbanas.id/138/">https://pmb.perbanas.id/138/</a>
-<a href="https://pusatprestasinasional.kemdikbud.go.id/nasgor/">https://pusatprestasinasional.kemdikbud.go.id/nasgor/</a>
-<a href="https://jurnal.umsu.ac.id/nasgor/">https://jurnal.umsu.ac.id/nasgor/</a>
-<a href="https://e-journal.stikessatriabhakti.ac.id/rajasloto/">https://e-journal.stikessatriabhakti.ac.id/rajasloto/</a>
-<a href="https://e-journal.stikessatriabhakti.ac.id/taiwan/">https://e-journal.stikessatriabhakti.ac.id/taiwan/</a>
-<a href="https://e-journal.stikessatriabhakti.ac.id/burungbet/">https://e-journal.stikessatriabhakti.ac.id/burungbet/</a>
-<a href="https://si.panda.id/rajasloto/">https://si.panda.id/rajasloto/</a>
-<a href="https://si.panda.id/kamboja/">https://si.panda.id/kamboja/</a>
-<a href="https://si.panda.id/burungbet/">https://si.panda.id/burungbet/</a>
 </div>
