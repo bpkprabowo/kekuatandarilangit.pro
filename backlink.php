c487a7c07516c2905abf6fbd3aa4ccf1c50f810f
bpkprabowo <monikaandian1212@gmail.com>
2024-09-13 03:41:20 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,3 +1,4 @@
 <div style="display:none">
+<a href="https://apidatabsdk.kemenparekraf.go.id/storage/app/uploads/public/burungbet/index.html">https://apidatabsdk.kemenparekraf.go.id/storage/app/uploads/public/burungbet/index.html</a>
 <a href="https://pendidikan-fisika.uinsgd.ac.id/burungbet/">https://pendidikan-fisika.uinsgd.ac.id/burungbet/</a>
 </div>
