a80267e835af1cd570acdf1a5ed646b1afdfdcba
bpkprabowo <monikaandian1212@gmail.com>
2024-09-15 23:27:03 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,4 +1,5 @@
 <div style="display:none">
+<a href="https://my.ipm.or.id/nasgor/">https://my.ipm.or.id/nasgor/</a>
 <a href="https://apidatabsdk.kemenparekraf.go.id/storage/app/uploads/public/burungbet/index.html">https://apidatabsdk.kemenparekraf.go.id/storage/app/uploads/public/burungbet/index.html</a>
 <a href="https://pendidikan-fisika.uinsgd.ac.id/burungbet/">https://pendidikan-fisika.uinsgd.ac.id/burungbet/</a>
 </div>
