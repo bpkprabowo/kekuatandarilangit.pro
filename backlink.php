4044a2dbece8b6b8fd80db4c2237bfa114046e3e
bpkprabowo <monikaandian1212@gmail.com>
2024-10-05 07:43:39 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,17 +1,12 @@
 <div style="display:none">
+<a href="https://ejurnal.bkkbn.go.id/boost/">https://ejurnal.bkkbn.go.id/boost/</a>
 <a href="https://jurnal.sttekumene.ac.id/boost/">https://jurnal.sttekumene.ac.id/boost/</a>
 <a href="https://www.ejurnal.itsi.ac.id/boost/">https://www.ejurnal.itsi.ac.id/boost/</a>
 <a href="https://journal.stiba.ac.id/boost/">https://journal.stiba.ac.id/boost/</a>
 <a href="https://journal.inten.ac.id/boost/">https://journal.inten.ac.id/boost/</a>
 <a href="https://ejournal.widyayuwana.ac.id/public/site/boost/">https://ejournal.widyayuwana.ac.id/public/site/boost/</a>
-<a href="https://logbookpps.umpar.ac.id/boost/">https://logbookpps.umpar.ac.id/boost/</a>
-<a href="https://teknik.unwim.ac.id/boots/index.html">https://teknik.unwim.ac.id/boots/index.html</a>
-<a href="https://elearning.ulm.ac.id/burungbet/">https://elearning.ulm.ac.id/burungbet/</a>
 <a href="https://transportth.com/point/">https://transportth.com/point/</a>
 <a href="https://wahanamedia.com/burungbet/">https://wahanamedia.com/burungbet/</a>
 <a href="https://graduatenstru.com/4388-2/">https://graduatenstru.com/4388-2/</a>
 <a href="https://gurumengajar.id/burung/">https://gurumengajar.id/burung/</a>
-<a href="https://inbis.unhi.ac.id/about/">https://inbis.unhi.ac.id/about/</a>
-<a href="https://proceeding.uingusdur.ac.id/burungbet/">https://proceeding.uingusdur.ac.id/burungbet/</a>
-<a href="https://logbookpps.umpar.ac.id/burungbet/">https://logbookpps.umpar.ac.id/burungbet/</a>
 </div>
