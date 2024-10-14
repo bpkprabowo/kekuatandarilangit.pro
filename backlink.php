41021a33fc26444362ba1629a9623bb5a7dcf2b1
bpkprabowo <monikaandian1212@gmail.com>
2024-10-14 08:31:01 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,12 +1,8 @@
 <div style="display:none">
+<a href="https://jurnal.itpln.ac.id/boost/">https://jurnal.itpln.ac.id/boost/</a>
 <a href="https://ejurnal.bkkbn.go.id/boost/">https://ejurnal.bkkbn.go.id/boost/</a>
 <a href="https://jurnal.sttekumene.ac.id/boost/">https://jurnal.sttekumene.ac.id/boost/</a>
 <a href="https://www.ejurnal.itsi.ac.id/boost/">https://www.ejurnal.itsi.ac.id/boost/</a>
 <a href="https://journal.stiba.ac.id/boost/">https://journal.stiba.ac.id/boost/</a>
-<a href="https://journal.inten.ac.id/boost/">https://journal.inten.ac.id/boost/</a>
-<a href="https://ejournal.widyayuwana.ac.id/public/site/boost/">https://ejournal.widyayuwana.ac.id/public/site/boost/</a>
-<a href="https://transportth.com/point/">https://transportth.com/point/</a>
 <a href="https://wahanamedia.com/burungbet/">https://wahanamedia.com/burungbet/</a>
-<a href="https://graduatenstru.com/4388-2/">https://graduatenstru.com/4388-2/</a>
-<a href="https://gurumengajar.id/burung/">https://gurumengajar.id/burung/</a>
 </div>
