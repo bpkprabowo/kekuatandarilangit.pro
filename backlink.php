15307cdb10c292fd18ca5701f73e5305c67f270e
bpkprabowo <monikaandian1212@gmail.com>
2024-10-14 08:31:40 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,4 +1,5 @@
 <div style="display:none">
+<a href="https://sice.umsida.ac.id/boost/">https://sice.umsida.ac.id/boost/</a>
 <a href="https://jurnal.itpln.ac.id/boost/">https://jurnal.itpln.ac.id/boost/</a>
 <a href="https://ejurnal.bkkbn.go.id/boost/">https://ejurnal.bkkbn.go.id/boost/</a>
 <a href="https://jurnal.sttekumene.ac.id/boost/">https://jurnal.sttekumene.ac.id/boost/</a>
