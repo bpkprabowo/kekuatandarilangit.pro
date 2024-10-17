243f88d9931e576be00e5617403cc602d5cbe661
bpkprabowo <monikaandian1212@gmail.com>
2024-10-17 21:47:14 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,16 +1,7 @@
 <div style="display:none">
 <a href="https://www.traigiunqueght.vn/">https://www.traigiunqueght.vn/</a>
 <a href="https://www.hoangthienduc.com/">https://www.hoangthienduc.com/</a>
-<a href="https://www.kidneytreatment.help/sites/default/files/pictures/">https://www.kidneytreatment.help/sites/default/files/pictures/</a>
-<a href="https://www.addictiontreatment.help/sites/default/files/boost/">https://www.addictiontreatment.help/sites/default/files/boost/</a>
-<a href="https://www.bollywoodupdates.com/boost/">https://www.bollywoodupdates.com/boost/</a>
-<a href="https://www.veganismworld.com/sites/default/files/styles/">https://www.veganismworld.com/sites/default/files/styles/</a>
-<a href="https://wahanamedia.com/turbo/">https://wahanamedia.com/turbo/</a>
 <a href="https://sice.umsida.ac.id/boost/">https://sice.umsida.ac.id/boost/</a>
-<a href="https://jurnal.itpln.ac.id/boost/">https://jurnal.itpln.ac.id/boost/</a>
 <a href="https://ejurnal.bkkbn.go.id/boost/">https://ejurnal.bkkbn.go.id/boost/</a>
-<a href="https://jurnal.sttekumene.ac.id/boost/">https://jurnal.sttekumene.ac.id/boost/</a>
-<a href="https://www.ejurnal.itsi.ac.id/boost/">https://www.ejurnal.itsi.ac.id/boost/</a>
-<a href="https://journal.stiba.ac.id/boost/">https://journal.stiba.ac.id/boost/</a>
 <a href="https://wahanamedia.com/burungbet/">https://wahanamedia.com/burungbet/</a>
 </div>
