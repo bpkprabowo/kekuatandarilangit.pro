72b07425c09542feb837bcdbe80fcca027a632cb
bpkprabowo <monikaandian1212@gmail.com>
2024-10-28 01:22:50 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -18,19 +18,5 @@
 <a href="https://akuntansi.pnp.ac.id/turbo/">https://akuntansi.pnp.ac.id/turbo/</a>
 <a href="https://tbi.uinsgd.ac.id/boost/">https://tbi.uinsgd.ac.id/boost/</a>
 <a href="https://landing-alwise.bithouse.id/boost/">https://landing-alwise.bithouse.id/boost/</a>
-<a href="https://e-arsip.appsakademi.id/boost/">https://e-arsip.appsakademi.id/boost/</a>
-<a href="https://journal.stisalmanar.ac.id/">https://journal.stisalmanar.ac.id/</a>
-<a href="https://ejournal.stikesjypr.ac.id/">https://ejournal.stikesjypr.ac.id/</a>
-<a href="https://arsiv.omu.edu.tr/turbo/">https://arsiv.omu.edu.tr/turbo/</a>
-<a href="https://jtridharma.ub.ac.id/">https://jtridharma.ub.ac.id/</a>
-<a href="https://prosidingmami.ub.ac.id/">https://prosidingmami.ub.ac.id/</a>
-<a href="https://stikesyahoedsmg.ac.id/boost/">https://stikesyahoedsmg.ac.id/boost/</a>
-<a href="https://cosy.akademisi.co.id/boost/">https://cosy.akademisi.co.id/boost/</a>
-<a href="https://sisakti.konijateng.id/">https://sisakti.konijateng.id/</a>
-<a href="https://siap.hanura.or.id/">https://siap.hanura.or.id/</a>
-<a href="https://www.traigiunqueght.vn/">https://www.traigiunqueght.vn/</a>
-<a href="https://www.hoangthienduc.com/">https://www.hoangthienduc.com/</a>
-<a href="https://sice.umsida.ac.id/boost/">https://sice.umsida.ac.id/boost/</a>
 <a href="https://ejurnal.bkkbn.go.id/boost/">https://ejurnal.bkkbn.go.id/boost/</a>
-<a href="https://wahanamedia.com/burungbet/">https://wahanamedia.com/burungbet/</a>
 </div>
